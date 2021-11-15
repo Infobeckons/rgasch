c96995a5466d01ba1b33337f2baf7c166b0d2d1d
manjeet12-ssd <manjeetsingh2632@gmail.com>
2021-11-15 11:15:55 +0530

    cookies and response cache

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,6 +26,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+        $schedule->command('media-library:delete-old-temporary-uploads')->daily();
     }
 
     /**
