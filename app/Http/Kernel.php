4fd49729f62a58cff40d48ca070fd2fb606bd013
manjeet12-ssd <manjeetsingh2632@gmail.com>
2021-11-15 12:06:44 +0530

    cookie middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -20,6 +20,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
         \App\Http\Middleware\HttpsRedirect::class,
+        \Spatie\CookieConsent\CookieConsentMiddleware::class,
     ];
 
     /**
